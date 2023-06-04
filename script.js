f629a4d08e6f695e677c07dfb03ca8408baf70a4
Siv0k <artvlad2003@gmail.com>
2023-06-04 22:30:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let table = document.createElement('table');
+for (i = 0; i < 9; i++) {
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (j = 0; j < 9; j++) {
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = '';
+    td.appendChild(button);
+    tr.appendChild(td);
+    }
+}
+document.body.appendChild(table);
+
+function Lkm() {
+let button = document.querySelectorAll('button');
+for (i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function() {
+        this.textContent = 'X';
+    });
+}
+}
+
+function Pkm() {
+    let button = document.querySelectorAll('button');
+    for (i = 0; i < button.length; i++) {
+        button[i].addEventListener('contextmenu', function() {
+            this.textContent = 'O';
+        });
+}
+}
+
+function PlaceMines(BoardSize, Mines) {
+let button = document.querySelectorAll('button');
+}
+
+Lkm();
+Pkm();
