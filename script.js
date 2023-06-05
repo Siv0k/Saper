5059157f0d5a1ab2a3639d839f7cec38d5b04805
Siv0k <artvlad2003@gmail.com>
2023-06-05 19:17:15 +0300

    When changing the size of the board, the cells are no longer added to the previous ones, but are displayed anew

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let gameBoard = [];
 function GetBoardSize() {
-    let boardSize = document.querySelector('input').value;
-    gameBoard.length = boardSize;
+    if (gameBoard.length != 0) {
+        let buttons = document.querySelectorAll('div > button');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].remove();
+        }
+        gameBoard.length = 0;
+        let boardSize = document.querySelector('input').value;
+        gameBoard.length = boardSize;
+    } else {
+        let boardSize = document.querySelector('input').value;
+        gameBoard.length = boardSize;
+    }
     for (let i = 0; i < gameBoard.length; i++) {
         GameBoard.insertAdjacentHTML('beforeend', '<button></button>');
     }
